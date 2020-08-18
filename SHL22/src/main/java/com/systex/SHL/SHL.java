f838b3b853d3fe50fcb531962b6bb448988f0505
2000159 <tlksimba519@gmail.com>
2020-08-18 14:51:42 +0800

    調整decodeOld&startSHL流程

--- SHL22/src/main/java/com/systex/SHL/SHL.java
+++ SHL22/src/main/java/com/systex/SHL/SHL.java
@@ -5,22 +5,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.systex.Main;
-
 public class SHL {
-	public static String processSourceDirectory;
+	public static String processSourceDirectory = "E:\\";
 	public static String inSecurityNo;
 	private String[] fileType = { "PSM", "PSL", "PLA", "PSR", "PID", "PCO", "LMK", "RAE", "PEL", "DRF", "CPM", "RSA" };
-	private Logger Logger = LoggerFactory.getLogger(Main.class);
+//	private Logger Logger = LoggerFactory.getLogger(Main.class);
 	private unity shlFunction = new unity();
 	private Map ptrData;
+	private List PSMData;
+	private List PSLData;
+	private List PLAData;
+	private List PSRData;
+	private List PIDData;
+	private List PCOData;
+	private List LMKData;
+	private List RAEData;
+	private List PELData;
+	private List DRFData;
+	private List CPMData;
+	private List RSAData;
 
 	public SHL(String processSourceDirectory, String inSecurityNo) {
 		this.processSourceDirectory = processSourceDirectory;
@@ -28,20 +36,76 @@ public class SHL {
 	}
 
 	public String startSHL() {
-		
-		setConfig();
-		
-		try {
-			
-			ptrData = readPTR();
+		PSMData = new ArrayList<List<String>>();
+		PSLData = new ArrayList<List<String>>();
+		PLAData = new ArrayList<List<String>>();
+		PSRData = new ArrayList<List<String>>();
+		PIDData = new ArrayList<List<String>>();
+		PCOData = new ArrayList<List<String>>();
+		LMKData = new ArrayList<List<String>>();
+		RAEData = new ArrayList<List<String>>();
+		PELData = new ArrayList<List<String>>();
+		DRFData = new ArrayList<List<String>>();
+		CPMData = new ArrayList<List<String>>();
+		RSAData = new ArrayList<List<String>>();
 
-			List pslData = shlFunction.readFile(inSecurityNo + ".PSL");
-			if(!shlFunction.compareRecords((int)ptrData.get("PSL"), pslData.size(), inSecurityNo + ".PSL")) {
-				return "ER";
+		setConfig();
+
+		try {
+			ptrData = readPTR();
+			// 走訪所有檔案一次檢查完成
+			for (String type : fileType) {
+				List uncheckedFile = shlFunction.readFile(inSecurityNo + "." + type);
+				if (!shlFunction.compareRecords((int) ptrData.get(type), uncheckedFile.size(),
+						inSecurityNo + "." + type)) {
+					return "ER";
+				}
+				for (Object s : uncheckedFile) {
+					List targetData = new ArrayList<List<String>>();
+					targetData.add(shlFunction.decodeOld((String) s, type));
+					switch (type) {
+					case "PSM":
+						PSMData = targetData;
+						break;
+					case "PSL":
+						PSLData = targetData;
+						break;
+					case "PLA":
+						PLAData = targetData;
+						break;
+					case "PSR":
+						PSRData = targetData;
+						break;
+					case "PID":
+						PIDData = targetData;
+						break;
+					case "PCO":
+						PCOData = targetData;
+						break;
+					case "LMK":
+						LMKData = targetData;
+						break;
+					case "RAE":
+						RAEData = targetData;
+						break;
+					case "PEL":
+						PELData = targetData;
+						break;
+					case "DRF":
+						DRFData = targetData;
+						break;
+					case "CPM":
+						CPMData = targetData;
+						break;
+					case "RSA":
+						RSAData = targetData;
+						break;
+					}
+				}
 			}
-			
-			
 		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("X");
 			return "ER";
 		}
 		return "OK";
@@ -67,7 +131,7 @@ public class SHL {
 				dataNow = br.readLine().substring(14).trim();
 				if (dataNow.equals("")) {
 					// 寫LOG
-					Logger.error("*ERR* PTR 缺資料" + inSecurityNo + ".PTR");
+//					Logger.error("*ERR* PTR 缺資料" + inSecurityNo + ".PTR");
 					throw new NullPointerException();
 				} else {
 					PTRRecords.put(s, Integer.parseInt(dataNow));
@@ -76,7 +140,7 @@ public class SHL {
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 			// 寫LOG
-			Logger.error("*ERR* PTR下載筆數非數字。" + inSecurityNo + ".PTR" + " 檔案內容：" + dataNow);
+//			Logger.error("*ERR* PTR下載筆數非數字。" + inSecurityNo + ".PTR" + " 檔案內容：" + dataNow);
 			throw e;
 		} catch (NullPointerException e) {
 			throw e;
--- SHL22/src/main/java/com/systex/SHL/unity.java
+++ SHL22/src/main/java/com/systex/SHL/unity.java
@@ -1,50 +1,60 @@
 package com.systex.SHL;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.systex.Main;
-
 public class unity {
+	private final static int[] PSMIndex = { 11, 19, 10, 39, 39, 13, 13, 1, 7, 9, 8, 6, 1, 8, 20, 1, 1, 1, 3, 17, 10, 1,
+			1, 6, 2, 1, 1 };
+	private final static int[] PSLIndex = { 11, 10, 40, 39, 6 };
+	private final static int[] PLAIndex = { 6, 11, 19, 10, 39, 11, 19, 10, 39, 13, 1, 11, 1, 1, 11, 1 };
+	private final static int[] PSRIndex = { 6, 10, 1, 13, 13, 13, 19, 1, 10, 20, 1 };
+	private final static int[] PIDIndex = { 11, 19, 10, 39, 39, 13, 13, 1, 7, 9, 8, 6, 1, 8, 20, 1, 1, 1, 3, 17, 10, 1,
+			1 };
+	private final static int[] PCOIndex = { 6, 1, 11, 19, 10, 39, 39, 13, 13, 1, 7, 9, 8, 8, 20, 1, 2, 3, 17 };
+	private final static int[] LMKIndex = { 6, 11, 19, 10, 39, 39, 13, 1, 7, 9, 8, 8, 20, 3, 17 };
+	private final static int[] RAEIndex = { 11, 19, 10, 39, 39, 9, 6 };
+	private final static int[] PELIndex = { 11, 10, 300, 6 };
+	private final static int[] DRFIndex = { 6, 11, 19, 10, 39, 11, 19, 10, 39, 13, 10, 19, 10, 21, 1 };
+	private final static int[] CPMIndex = { 11, 19, 10, 6, 19, 10, 8, 39, 39, 20, 11, 1, 13, 29, 13, 1, 13 };
+	private final static int[] RSAIndex = { 6, 11, 10, 19, 13 };
 
-	private Logger Logger = LoggerFactory.getLogger(Main.class);
+//	private Logger Logger = LoggerFactory.getLogger(Main.class);
 
-	public boolean compareRecords(int ptrData,int records,String fileName) throws Exception {
+	public boolean compareRecords(int ptrData, int records, String fileName) throws Exception {
 		try {
 			if (ptrData != records) {
-				Logger.error("*ERR* {} 筆數不符  PRT-檔案筆數：{} 下載筆數：{}",fileName,ptrData,records);
+//				Logger.error("*ERR* {} 筆數不符  PTR-檔案筆數：{} 下載筆數：{}", fileName, ptrData, records);
 				return false;
 			} else {
 				System.out.println("*MSG* " + fileName + "資料筆數檢核正確，下載筆數：" + records);
 				// 寫LOG
-				Logger.info("*MSG* " + fileName + "資料筆數檢核正確，下載筆數為：" + records);
+//				Logger.info("*MSG* " + fileName + "資料筆數檢核正確，下載筆數為：" + records);
 				return true;
 			}
 		} catch (Exception e) {
-			Logger.error("Error");
+//			Logger.error("Error");
 			throw e;
 		}
 	}
-	
-	public List readFile (String fileName) throws Exception{
+
+	public List readFile(String fileName) throws Exception {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(
-				new FileInputStream(new File(SHL.processSourceDirectory + fileName)), "Big5"));) {
+				new FileInputStream(new File(SHL.processSourceDirectory + fileName)), "UTF-8"));) {
 			String temp;
 			List targetData = new ArrayList();
 			while ((temp = br.readLine()) != null) {
 				targetData.add(temp);
 			}
 			return targetData;
-		}catch(FileNotFoundException e) {
-			Logger.error("*ERR* 缺 PSL 檔" + fileName + "FILE STATUS : " + e.getMessage());
+		} catch (FileNotFoundException e) {
+			// 要改彈性?
+//			Logger.error("*ERR* 缺 PSL 檔" + fileName + "FILE STATUS : " + e.getMessage());
 			throw e;
 		}
 	}
@@ -57,11 +67,50 @@ public class unity {
 		return response;
 	}
 
-	public List<String> decodeOld(String request, int[] split) {
+	public List<String> decodeOld(String request, String type) {
+		int[] split = null;
+		switch (type) {
+		case "PSM":
+			split = PSMIndex;
+			break;
+		case "PSL":
+			split = PSLIndex;
+			break;
+		case "PLA":
+			split = PLAIndex;
+			break;
+		case "PSR":
+			split = PSRIndex;
+			break;
+		case "PID":
+			split = PIDIndex;
+			break;
+		case "PCO":
+			split = PCOIndex;
+			break;
+		case "LMK":
+			split = LMKIndex;
+			break;
+		case "RAE":
+			split = RAEIndex;
+			break;
+		case "PEL":
+			split = PELIndex;
+			break;
+		case "DRF":
+			split = DRFIndex;
+			break;
+		case "CPM":
+			split = CPMIndex;
+			break;
+		case "RSA":
+			split = RSAIndex;
+			break;
+		}
 		List response = new ArrayList();
 		int index = 0;
 		for (int position : split) {
-			response.add(request.substring(index, index + position));
+			response.add(request.substring(index, index + position).replace("　", "").replace(" ", ""));
 			index += position;
 		}
 		return response;
